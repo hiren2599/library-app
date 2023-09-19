123bc792cb6865b53005a50eb612f7e54fcc2ad4
hiren <hirenlineshbhaipatel@gmail.com>
2023-09-19 15:22:43 +0530

    Added Carousel and Explore Top Books Component on Home Page

--- 03-frontend/react-library/src/App.tsx
+++ 03-frontend/react-library/src/App.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import "./App.css";
 import { Navbar } from "./Layouts/NavbarAndFooter/Navbar";
+import { ExploreTopBooks } from "./Layouts/HomePage/ExploreTopBooks";
+import { Carousel } from "./Layouts/HomePage/Carousel";
 
 function App() {
-	return <Navbar />;
+	return (
+		<div>
+			<Navbar />
+			<ExploreTopBooks />
+			<Carousel />
+		</div>
+	);
 }
 
 export default App;
