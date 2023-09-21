79f2887ca23e5e291dda010ad9d6bccd0897b0c8
hiren <hirenlineshbhaipatel@gmail.com>
2023-09-21 12:40:05 +0530

    Added Routing to Front End

--- 03-frontend/react-library/src/App.tsx
+++ 03-frontend/react-library/src/App.tsx
@@ -3,13 +3,25 @@ import { Navbar } from "./Layouts/NavbarAndFooter/Navbar";
 import { Footer } from "./Layouts/NavbarAndFooter/Footer";
 import { HomePage } from "./Layouts/HomePage/HomePage";
 import { SearchBooksPage } from "./Layouts/SearchBooksPage/SearchBooksPage";
+import { Redirect, Route, Switch } from "react-router-dom";
 
 export const App = () => {
 	return (
-		<div>
+		<div className="d-flex flex-column min-vh-100">
 			<Navbar />
-			{/* <HomePage /> */}
-			<SearchBooksPage />
+			<div className="flex-grow-1">
+				<Switch>
+					<Route path="/" exact>
+						<Redirect to="/home" />
+					</Route>
+					<Route path="/home" exact>
+						<HomePage />
+					</Route>
+					<Route path="/search">
+						<SearchBooksPage />
+					</Route>
+				</Switch>
+			</div>
 			<Footer />
 		</div>
 	);
--- 03-frontend/react-library/src/Layouts/HomePage/components/Carousel.tsx
+++ 03-frontend/react-library/src/Layouts/HomePage/components/Carousel.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { ReturnBook } from "./ReturnBook";
 import BookModel from "../../../models/BookModel";
 import { SpinnerLoading } from "../../Utils/SpinnerLoading";
+import { Link } from "react-router-dom";
 
 export const Carousel = () => {
 	const [books, setBooks] = useState<BookModel[]>([]);
@@ -124,9 +125,9 @@ export const Carousel = () => {
 				</div>
 			</div>
 			<div className="homepage-carousel-title mt-3">
-				<a className="btn btn-outline-secondary btn-lg" href="#">
+				<Link className="btn btn-outline-secondary btn-lg" to="/search">
 					View More
-				</a>
+				</Link>
 			</div>
 		</div>
 	);
